39c8f80d93c8817536d2729387342832d9120f02
Nathan Harlé <nathan.harle@azursoft.com>
2021-06-14 17:34:34 +0200

    correct name requirements.txt \ add asset

--- discord_bot/model.py
+++ discord_bot/model.py
@@ -13,14 +13,21 @@ character_skill = Table(
     Column("degre", Integer)
 )
 
-class User(Base):
+character_asset = Table(
+    "character_asset",
+    Base.metadata,
+    Column("character_id", Integer, ForeignKey("character.id")),
+    Column("asset_id", Integer, ForeignKey("asset.id")),
+)
+
+class UserModel(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     user_name = Column(String)
     user_discord_id = Column(Integer)
     characters = relationship("character", backref=backref("user"))
 
-class Character(Base):
+class CharacterModel(Base):
     __table__ = "character"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"))
@@ -32,9 +39,15 @@ class Character(Base):
     wisdom = Column(Integer)
     charisma = Column(Integer)
 
-class Skill(Base):
+class SkillModel(Base):
     __table__ = "skill"
     id = Column(Integer, primary_key=True)
     skill_name = Column(String)
     skill_characteristic_1 = Column(String)
     skill_characteristic_2 = Column(String)
+
+class AssetModel(Base):
+    __table__ = "asset"
+    id = Column(Integer, primary_key=True)
+    asset_name = Column(String)
+    asset_description = Column(String)
